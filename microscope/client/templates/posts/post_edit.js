5b796ba5cab8401a96501484ff833e2c3a0cb94a
Ryan Peden <ryan04@gmail.com>
2014-12-18 11:30:56 -0500

    added delete handler for post edit page

--- microscope/client/templates/posts/post_edit.js
+++ microscope/client/templates/posts/post_edit.js
@@ -21,5 +21,11 @@ Template.postEdit.events({
 
 	'click .delete': function(e) {
 		e.preventDefault();
+
+		if (confirm("Delete this post?")){
+			var currentPostId = this._id;
+			Posts.remove(currentPostId);
+			Router.go('postsList');
+		}
 	}
 });
