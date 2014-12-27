d42c706e864968ad298011d830f6e8591d338070
Ryan Peden <ryan04@gmail.com>
2014-12-27 15:51:57 -0500

    added remove permission to posts collection

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,7 +1,8 @@
 Posts = new Mongo.Collection('posts');
 
 Posts.allow({
-	update: function(userId, post) { return ownsDocument(userId, post); }
+	update: function(userId, post) { return ownsDocument(userId, post); },
+	remove: function(userId, post) { return ownsDocument(userId, post); }
 });
 
 Meteor.methods({
